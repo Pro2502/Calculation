4ac76cbeca9675719223d2de63da7b45a2e91e0b
Anna <anna.propisnova@list.ru>
2023-01-14 18:07:46 +0300

    -Initial

---
+++ Getting_and_calculation_an_expression.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    class Getting_and_calculation_an_expression
+    {
+       double variable;
+        int max_priority;
+        List<string> operations = new List<string> {"!", "+", "-", "%", "/", "*", "^" };
+        public void Initialisation(ref string s)
+        {
+            Console.WriteLine("The output of the calculations and the result");
+            string[] subs = s.Split(' ');
+            bool condition = true;
+            while (condition)
+            {
+                max_priority = 0;
+                foreach (var sub in subs)
+                {
+                    bool result = double.TryParse(sub, out variable);
+                    if (result)
+                    {
+                    }
+                    else
+                    {
+                        for (int i = 0; i < operations.Count; i++)
+                        {
+                            if (operations[i] == sub && i > max_priority)
+                            {
+                                max_priority = i;
+                            }
+                        }
+                    }
+                }
+                if (max_priority == 0)
+                {
+                    condition = false;
+                    Array.Resize(ref subs, subs.Length - 2);
+                    Console.WriteLine("Answer : ");
+                    Console.WriteLine(String.Join(" ", subs));
+                    break;
+                }
+                else
+                {
+                    for (int k = 0; k < subs.Length; k++)
+                    {
+                        double result;
+                        if (subs[k] == operations[max_priority])
+                        {
+                            switch (subs[k])
+                            {
+                                case "+":
+                                    result = double.Parse(subs[k - 1]) + double.Parse(subs[k + 1]);
+                                    subs[k] = Convert.ToString(result);
+                                    break;
+                                case "-":
+                                    result = double.Parse(subs[k - 1]) - double.Parse(subs[k + 1]);
+                                    subs[k] = Convert.ToString(result);
+                                    break;
+                                case "%":
+                                    if (double.Parse(subs[k + 1]) != 0.0)
+                                    {
+                                        result = double.Parse(subs[k - 1]) % double.Parse(subs[k + 1]);
+                                        subs[k] = Convert.ToString(result);
+                                    }
+                                    else
+                                        Console.WriteLine(" На ноль не делится! Ошибка!");
+                                    break;
+                                case "*":
+                                    result = double.Parse(subs[k - 1]) * double.Parse(subs[k + 1]);
+                                    subs[k] = Convert.ToString(result);
+                                    break;
+                                case "/":
+                                    if (double.Parse(subs[k + 1]) != 0.0)
+                                    {
+                                        result = double.Parse(subs[k - 1]) / double.Parse(subs[k + 1]);
+                                        subs[k] = Convert.ToString(result);
+                                    }
+                                    else
+                                        Console.WriteLine(" На ноль не делится! Ошибка!");
+                                    break;
+                                case "^":
+                                    result = Math.Pow(double.Parse(subs[k - 1]), double.Parse(subs[k + 1]));
+                                    subs[k] = Convert.ToString(result);
+                                    break;
+                                default:
+                                    Console.WriteLine("Неправильная операция!");
+                                    break;
+
+                            }
+
+                            for (int j = (k - 1); j < subs.Length - 1; j++)
+                            {
+                                subs[j] = subs[j + 1];
+                            }
+                            Array.Resize(ref subs, subs.Length - 1);
+
+                            for (int j = k; j < subs.Length - 1; j++)
+                            {
+                                subs[j] = subs[j + 1];
+                            }
+                            Array.Resize(ref subs, subs.Length - 1);
+
+                            Console.WriteLine(String.Join(" ", subs));
+                        }
+                    }
+                }
+            }
+        }       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Using operators:");
+            Console.WriteLine("'+' addition\n'-' subtraction\n'*' multiplication\n'/' division\n'^' degree\n'%' remainder of the division\n");
+            Console.WriteLine("write your expression separated by spaces:\n");
+            string s = Console.ReadLine();
+
+            Getting_and_calculation_an_expression getting_and_calculation_an_expression = new Getting_and_calculation_an_expression();
+            getting_and_calculation_an_expression.Initialisation(ref s);
+        }
+    }
+}
